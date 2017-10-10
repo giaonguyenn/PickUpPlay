04d1d2ab4400aadd05b0b069dca60d37ba1d436a
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-10-10 14:29:21 -0500

    done with front-end for now 10/10 2:29

--- public/js/mainSrvc.js
+++ public/js/mainSrvc.js
@@ -2,6 +2,7 @@ angular.module("PickUpPlayApp").service("mainSrvc", function($http) {
 
 	var baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="
 	var apiKey = "AIzaSyDx3NmAEho6hkw_NSTWBZU3EFadH87jxRs";
+	var map;
 	
 	this.searchAddress = function() {
 		$(".searchbyAddress").css("display", "initial");
@@ -18,7 +19,7 @@ angular.module("PickUpPlayApp").service("mainSrvc", function($http) {
 
 			function initMap () {
 				var location = object;
-		        var map = new google.maps.Map(document.getElementById('map'), {
+		        map = new google.maps.Map(document.getElementById('map'), {
 		          	zoom: 11,
 		          	center: location
 		        });
@@ -26,15 +27,39 @@ angular.module("PickUpPlayApp").service("mainSrvc", function($http) {
 		          	position: location,
 		          	map: map
 		        });
-
 		        var pyrmont = new google.maps.LatLng(object.lat,object.lng);
 		        	service = new google.maps.places.PlacesService(map);
-					service.nearbySearch({location: pyrmont, keyword: "basketball courts", radius: 5000}, (res) => {
-						console.log(res);				});
+					service.nearbySearch({location: pyrmont, keyword: "volleyball courts", radius: 5000}, callback
+				);
 		    };
 
-		    initMap();
+		    function callback(results, status) {
+		        if (status === google.maps.places.PlacesServiceStatus.OK) {
+		        	for (var i = 0; i < results.length; i++) {
+		           		createMarker(results[i]);
+		          	}
+		        }
+		    };
 
+	      	function createMarker(place) {
+	      		// console.log(place);
+	        	var placeLoc = {lat: place.geometry.location.lat(), lng: place.geometry.location.lng()};
+	        	var marker = new google.maps.Marker({
+	          		map: map,
+	          		position: placeLoc
+	        	});
+
+				var contentString = place.name + " - " + place.rating + "</br></br>" + place.vicinity + "</br> </br>";
+
+        		var infowindow = new google.maps.InfoWindow({});
+
+	        	google.maps.event.addListener(marker, 'click', function() {
+	          		infowindow.setContent(contentString);
+	          		infowindow.open(map, this);
+	        	});
+	      	}
+
+      		initMap();
 		});
 	};
 
