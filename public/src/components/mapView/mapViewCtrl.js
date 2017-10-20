35ef4696f1f74d5a2124604a1b82576e27193bb7
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-10-20 15:24:54 -0500

    deploying to Heroku?!

--- public/src/components/mapView/mapViewCtrl.js
+++ public/src/components/mapView/mapViewCtrl.js
@@ -1,5 +1,9 @@
 angular.module("PickUpPlayApp").controller("mapViewCtrl", function($scope, mainSrvc, $state) {
 
+	angular.element(document).ready(function(){
+	  $("navbar").css("display", "initial");
+	});
+
 	const {zip,searchTerm} = $state.params;
 
 	if(zip, searchTerm) {
