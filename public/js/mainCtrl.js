0b197eedd04a4361cce24ebf779dc143d917f4be
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-10-22 13:08:57 -0500

    10/22 1:08p-styled sign up page, working on profile page

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -10,19 +10,19 @@ angular.module("PickUpPlayApp").controller("mainCtrl", function($scope, mainSrvc
       $state.go("searchBy");
   	})
     .catch((err) => {
-      alert(err);
-      'use strict';
+      // alert(err);
+      // 'use strict';
 
-      window.addEventListener('load', function() {
-        var form = document.getElementById('needs-validation');
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      }, false);
+      // window.addEventListener('load', function() {
+      //   var form = document.getElementById('needs-validation');
+      //   form.addEventListener('submit', function(event) {
+      //     if (form.checkValidity() === false) {
+      //       event.preventDefault();
+      //       event.stopPropagation();
+      //     }
+      //     form.classList.add('was-validated');
+      //   }, false);
+      // }, false);
     });
   };
 
