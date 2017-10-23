15add73fce955148e040e7eb38e33c8f0e3028e8
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-10-22 21:16:35 -0500

    10/22 9:16p- updated styling

--- public/src/components/mapView/mapViewCtrl.js
+++ public/src/components/mapView/mapViewCtrl.js
@@ -1,9 +1,5 @@
 angular.module("PickUpPlayApp").controller("mapViewCtrl", function($scope, mainSrvc, $state) {
 
-	angular.element(document).ready(function(){
-	  $("navbar").css("display", "initial");
-	});
-
 	const {zip,searchTerm} = $state.params;
 
 	if(zip, searchTerm) {
